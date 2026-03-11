7094cd813b873b0a93e6294b08f95e4e1bc1eed4
Mariane Santana da Silva <marianess638@gmail.com>
2026-03-11 15:01:12 -0300

    Clean up main method by removing exercises
    Removed multiple exercises and calculations from the main method.

---
+++ AlgoritmoProgramação1/ListaExercicio_1/Exercicio4.java
@@ -0,0 +1,19 @@
+//Mariane Santana da Silva RA:1143431607
+
+import java.util.Scanner;
+
+class Main {
+    public static void main (String [] args){
+        Scanner ler = new Scanner(System.in);
+
+        // 4° Exercício
+
+        System.out.println("1° numero:");
+        Double x = ler.nextDouble();
+
+        System.out.println("2° numero:");
+        Double y = ler.nextDouble();
+
+        System.out.printf("\nO produto destes numeros é %f\n",x*y);
+    }
+}
