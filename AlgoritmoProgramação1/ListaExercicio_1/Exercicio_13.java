9dc0594773a05ecfb8c5395995cf0d8a04a943c8
Mariane Santana da Silva <marianess638@gmail.com>
2026-03-11 15:14:03 -0300

    Add Exercicio_13.java to ListaExercicio_1

---
+++ AlgoritmoProgramação1/ListaExercicio_1/Exercicio_13.java
@@ -0,0 +1,23 @@
+//Mariane Santana da Silva RA:1143431607
+
+import java.lang.Math;
+import java.util.Scanner;
+
+class Main {
+    public static void main (String [] args){
+        Scanner ler = new Scanner(System.in);
+
+        //Exercício 13
+
+        System.out.println("Imforme um número: "); 
+        double num =e.nextDouble();
+        double Q=num*num;
+        double cubo=num*num*num;
+        double raiz=Math.sqrt(num);
+        double potencia=Math.pow(num, 10);
+        System.out.println("O quadrado deste numero é: "+quadrado);
+        System.out.println("O cubo deste numero é: "+cubo);
+        System.out.println("A raiz quadrada deste numero é: "+raiz);
+        System.out.println("Este numero elevado a decima potencia é: "+potencia);
+    }
+}
