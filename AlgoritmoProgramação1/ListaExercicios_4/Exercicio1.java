5f59e273fa17aa1a8e80062a97198ab8f20f6d72
Mariane Santana da Silva <marianess638@gmail.com>
2026-03-25 09:02:37 -0300

    Create Exercicio1.java

---
+++ AlgoritmoProgramação1/ListaExercicios_4/Exercicio1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int contador, resto;
+        contador = 100;
+
+        while (contador <=200){
+            System.out.println(contador+"");
+            contador++;
+        }
+    }
+}
