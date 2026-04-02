56b2fcd2b779d0088170257dbebfdcf176c9eae2
Mariane Santana da Silva <marianess638@gmail.com>
2026-04-02 19:57:43 -0300

    Add countdown program using for loop

---
+++ AlgoritmoProgramação1/ListaExercicios_4/Exercicio1_for.java
@@ -0,0 +1,16 @@
+//Mariane Santana da Silva RA:1143431607
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int contador=100;
+        
+        for(contador=100; contador>=1; contador--){
+                
+            System.out.println(+contador);
+        }
+    }
+}
