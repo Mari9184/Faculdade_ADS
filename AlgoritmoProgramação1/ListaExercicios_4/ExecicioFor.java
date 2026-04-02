cba462f8e8528d7c81ac3db9146f2a15bad33096
Mariane Santana da Silva <marianess638@gmail.com>
2026-04-02 20:15:16 -0300

    Create ExecicioFor.java

---
+++ AlgoritmoProgramação1/ListaExercicios_4/ExecicioFor.java
@@ -0,0 +1,16 @@
+//Mariane Santana da Silva RA:1143431607
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        int contador;
+        
+        for(contador=15; contador<=100; contador++){
+                
+            System.out.println("A raiz de:"+contador+" é "+contador*contador);
+        }
+    }
+}
