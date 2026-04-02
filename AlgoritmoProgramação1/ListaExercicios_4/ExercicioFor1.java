0a6b2b772e63b4de1026ffc134feb0ac519033eb
Mariane Santana da Silva <marianess638@gmail.com>
2026-04-02 20:21:34 -0300

    Update ExercicioFor1.java

--- AlgoritmoProgramação1/ListaExercicios_4/ExercicioFor1.java
+++ AlgoritmoProgramação1/ListaExercicios_4/ExercicioFor1.java
@@ -1,10 +1,7 @@
 //Mariane Santana da Silva RA:1143431607
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
 
         int contador=100;
         
