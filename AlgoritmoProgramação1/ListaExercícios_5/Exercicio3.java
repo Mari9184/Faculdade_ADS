e735296d6583b972e53f21c8c92f12f38d470ffa
Mariane Santana da Silva <marianess638@gmail.com>
2026-05-12 18:25:18 -0300

    Create Exercicio3.java

---
+++ AlgoritmoProgramação1/ListaExercícios_5/Exercicio3.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[]args) {
+	    Scanner ler = new Scanner(System.in);
+	    String[] name = new String[5];
+	    
+	    
+		
+		for (int i=0; i<5; i++){
+		    System.out.printf("\n %d° Informe um nome: ", i);
+            name[i] = ler.nextLine();
+		}
+		
+		System.out.println("\n_____ Nomes em Decrescente _____");
+		
+		for(int i=4; i>=0; i--){
+
+		    System.out.println(name[i]);
+		}
+		
+		System.out.println("\n_____ Nomes em Crescente _____");
+		
+		for(int i=0; i<5; i++){
+	    
+	    	System.out.println(name[i]);
+		}
+        
+        ler.close();
+	}
+}
