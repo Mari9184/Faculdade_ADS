adff34d9f9707424e42c91581cb9516a2ee8d3ab
Mariane Santana da Silva <marianess638@gmail.com>
2026-05-12 19:21:02 -0300

    Create Exercicio4.java

---
+++ AlgoritmoProgramação1/ListaExercícios_5/Exercicio4.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[]args) {
+		Scanner ler = new Scanner(System.in);
+		int[] VetorA = new int[5];
+		int[] VetorB = new int[5];
+
+
+
+		for (int i=0; i<5; i++) {
+			System.out.printf("\n %d° Informe um nome: ", i);
+			VetorA[i] = ler.nextInt();
+		}
+
+
+        for(int i=0; i<5; i++){
+	       VetorB[i] = VetorA[i] * 3;
+		}
+
+		System.out.println("\n_____ Multiplicando por 3 _____");
+
+		for(int i=0; i<5; i++) {
+
+			System.out.println(VetorB[i]);
+		}
+
+		ler.close();
+	}
+}
