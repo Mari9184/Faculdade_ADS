6ec2ee0367ce4543ae3b8fdc44bfa99f1a8b288b
Mariane Santana da Silva <marianess638@gmail.com>
2026-05-12 17:05:35 -0300

    Add Exercicio1.java for number input and output

---
+++ AlgoritmoProgramação1/ListaExercícios_5/Exercicio1.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[]args) {
+	    Scanner ler = new Scanner(System.in);
+	    int[] number = new int[10];
+	    
+		
+		for (int i=0; i<10; i++){
+		    System.out.println("\nInforme um numero:"+(i));
+            number[i] = ler.nextInt();
+		}
+		
+		System.out.println("\n_____ Imprimindo números _____");
+		    for(int i=0; i<10; i++){
+		    System.out.println(number[i]);
+		 }
+        
+        ler.close();
+	}
+}
