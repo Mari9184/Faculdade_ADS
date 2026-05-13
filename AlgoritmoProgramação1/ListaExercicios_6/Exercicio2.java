52b4f40fa1b4653637607d4cb158e2b2fe74be06
Mariane Santana da Silva <marianess638@gmail.com>
2026-05-13 12:42:26 -0300

    Create Exercicio2.java

---
+++ AlgoritmoProgramação1/ListaExercicios_6/Exercicio2.java
@@ -0,0 +1,15 @@
+//Mariane Santana da Silva RA:1143431607
+
+public class Main {
+    public static void main(String[] args) {
+        
+        NovaMensagem("parametro");
+        NovaMensagem("parametro1");
+        NovaMensagem("parametro2");
+        
+    }
+
+    public static void NovaMensagem(String Parametros){
+        System.out.println("Meu primeiro programa com funcoes e parametro: " +Parametros);
+    }
+}
