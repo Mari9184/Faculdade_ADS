64f830a621b1a603e3d78b668e64876a1ebb2bd3
Mariane Santana da Silva <marianess638@gmail.com>
2026-05-13 12:18:12 -0300

    Add ListaExercicio6.java with a simple message function

---
+++ AlgoritmoProgramação1/ListaExercicio6.java
@@ -0,0 +1,15 @@
+//Mariane Santana da Silva RA:1143431607
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Mensagem();
+        Mensagem(); 
+        Mensagem(); 
+
+    }
+
+    public static void Mensagem (){
+    System.out.println("Função em Java");
+    }
+}
